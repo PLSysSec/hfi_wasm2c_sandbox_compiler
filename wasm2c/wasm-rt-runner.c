e65b284ce2f2d76216010c9666a8bf25f31d5e46
Tal Garfinkel <talg@cs.ucsd.edu>
2022-10-07 13:56:26 -0700

    initialize uvwasi state, resolved with hfi changes.

--- wasm2c/wasm-rt-runner.c
+++ wasm2c/wasm-rt-runner.c
@@ -7,6 +7,7 @@
 #include <string.h>
 
 #include "wasm-rt.h"
+#include "uvwasi.h"
 
 #if defined(_WIN32)
 // Ensure the min/max macro in the header doesn't collide with functions in
@@ -94,6 +95,42 @@ typedef void (*wasi_rt_sys_init_t)();
 typedef wasm2c_sandbox_funcs_t (*get_info_func_t)();
 typedef void (*wasm2c_start_func_t)(void* sbx);
 
+void init_uvwasi_local(uvwasi_t * local_uvwasi_state, int argc, char const * argv[])
+{
+    uvwasi_options_t init_options;
+
+    //pass in standard descriptors
+    init_options.in = 0;
+    init_options.out = 1;
+    init_options.err = 2;
+    init_options.fd_table_size = 3;
+
+    //pass in args and environement
+    extern const char ** environ;
+    init_options.argc = argc;
+    init_options.argv = argv;
+    init_options.envp = (const char **) environ;
+
+    //no sandboxing enforced, binary has access to everything user does
+    init_options.preopenc = 2;
+    init_options.preopens = calloc(2, sizeof(uvwasi_preopen_t));
+
+    init_options.preopens[0].mapped_path = "/";
+    init_options.preopens[0].real_path = "/";
+    init_options.preopens[1].mapped_path = "./";
+    init_options.preopens[1].real_path = ".";
+
+    init_options.allocator = NULL;
+
+    uvwasi_errno_t ret = uvwasi_init(local_uvwasi_state, &init_options);
+
+    if (ret != UVWASI_ESUCCESS) {
+        printf("uvwasi_init failed with error %d\n", ret);
+        exit(1);
+    }
+}
+
+
 int main(int argc, char const* argv[]) {
   if (argc < 2) {
     printf(
@@ -132,6 +169,10 @@ int main(int argc, char const* argv[]) {
   wasm_rt_memory_t* memory = sandbox_info.get_wasm2c_memory(sandbox);
   wasm_rt_hfi_enable(memory);
 #endif
+
+  uvwasi_t local_uvwasi_state;
+  init_uvwasi_local(&local_uvwasi_state, argc, argv);
+  sandbox_info.init_uvwasi_state(sandbox, &local_uvwasi_state);
 
   wasm2c_start_func_t start_func =
       (wasm2c_start_func_t)symbol_lookup(library, "w2c__start");
--- wasm2c/wasm-rt.h
+++ wasm2c/wasm-rt.h
@@ -21,6 +21,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include "uvwasi.h"
 
 // This wasm2c always assume hfi is enabled
 #define WASM2C_HFI_ENABLED
@@ -196,6 +197,7 @@ typedef struct wasm_func_type_t {
 #define WASM2C_WASI_MAX_FDS 32
 typedef struct wasm_sandbox_wasi_data {
   wasm_rt_memory_t* heap_memory;
+  uvwasi_t * uvwasi;
 
   uint32_t tempRet0;
 
@@ -215,6 +217,7 @@ typedef struct wasm_sandbox_wasi_data {
 typedef void (*wasm_rt_sys_init_t)(void);
 typedef void* (*create_wasm2c_sandbox_t)(uint32_t max_wasm_pages);
 typedef void (*destroy_wasm2c_sandbox_t)(void* sbx_ptr);
+typedef void (*init_uvwasi_state_t)(void* sbx_ptr, uvwasi_t *);
 typedef void* (*lookup_wasm2c_nonfunc_export_t)(void* sbx_ptr,
                                                 const char* name);
 typedef uint32_t (*lookup_wasm2c_func_index_t)(void* sbx_ptr,
@@ -232,6 +235,7 @@ typedef wasm_rt_memory_t* (*get_wasm2c_memory_t)(void* sbx_ptr);
 typedef struct wasm2c_sandbox_funcs_t {
   wasm_rt_sys_init_t wasm_rt_sys_init;
   create_wasm2c_sandbox_t create_wasm2c_sandbox;
+  init_uvwasi_state_t init_uvwasi_state;
   destroy_wasm2c_sandbox_t destroy_wasm2c_sandbox;
   lookup_wasm2c_nonfunc_export_t lookup_wasm2c_nonfunc_export;
   lookup_wasm2c_func_index_t lookup_wasm2c_func_index;
